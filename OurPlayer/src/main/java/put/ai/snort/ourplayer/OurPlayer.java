ca4fc9f99af747bc5eb826a1859a3b37100f4a0d
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2014-01-11 10:07:48 +0100

    zmiana poma, dodany dep do loa, dostep do loa move

--- OurPlayer/src/main/java/put/ai/snort/ourplayer/OurPlayer.java
+++ OurPlayer/src/main/java/put/ai/snort/ourplayer/OurPlayer.java
@@ -6,9 +6,11 @@ package put.ai.snort.ourplayer;
 
 import java.util.List;
 import java.util.Random;
+
 import put.ai.snort.game.Board;
 import put.ai.snort.game.Move;
 import put.ai.snort.game.Player;
+import put.ai.snort.linesofaction.*;
 
 public class OurPlayer extends Player {
 
@@ -22,6 +24,13 @@ public class OurPlayer extends Player {
     @Override
     public Move nextMove(Board b) {
         List<Move> moves = b.getMovesFor(getColor());
+        
+        for (Move _move : moves) {
+          LoAMove move = (LoAMove) _move;
+          System.out.println("x:" + move.getDstX());
+          System.out.println(move);
+        }
+        
         return moves.get(random.nextInt(moves.size()));
     }
 }
